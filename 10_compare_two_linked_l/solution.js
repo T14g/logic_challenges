406a511fc7bb01a6e6841a2673e43d684f95cb2b
Tiago <ottiagos@gmail.com>
2021-07-07 23:52:15 -0300

    Compare 2 linked lists solution

--- 10_compare_two_linked_l/solution.js
+++ 10_compare_two_linked_l/solution.js
@@ -1,6 +1,19 @@
-// 1 if equal length and same values
-
 function CompareLists(llist1, llist2) {
+    let head1 = llist1;
+    let head2 = llist2;
 
+    while (head1 !== null && head2 !== null) {
+        if (head1.data === head2.data) {
+            head1 = head1.next;
+            head2 = head2.next;
+        } else {
+            return 0;
+        }
+    }
 
+    if (head1 === null && head2 === null) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
