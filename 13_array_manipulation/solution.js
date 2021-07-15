ab50d6d76543d6969b51c5b60d834c3ef2b93e66
Tiago <ottiagos@gmail.com>
2021-07-15 01:35:57 -0300

    Array Manipulation Prefix Summ

--- 13_array_manipulation/solution.js
+++ 13_array_manipulation/solution.js
@@ -1,35 +1,38 @@
-function arrayManipulation(n, queries, arr = []) {
+function arrayManipulation(n, queries) {
 
-    if(arr.length === 0){
-       for(let i = 0; i < n; i++){
-        arr[i] = 0;
-        } 
-    }
-    
-    if(queries.length > 0){
-        let start = queries[0][0] - 1;
-        let end = queries[0][1] - 1;
-        let summ = queries[0][2];
-        let localMax = 0;
-        
-        for(let i = start; i <= end; i++){
-           arr[i] += summ;
-           localMax = Math.max(localMax,)
-        }
-        
-        queries.shift();
-        arrayManipulation(n, queries, arr );
-        
-    }
-    
+    let outputArray = [];
     let max = 0;
-    console.log(arr);
-    arr.map(num => {
-        if(num > max){
-            max = num;
-        }
-    })
-    
-    return max;
+    let summ = 0;
 
+    //Init array with 0's
+    for (let i = 0; i < n + 2; i++) {
+        outputArray[i] = 0;
+    }
+
+    //Prepare the array for the Prefix Summ
+    for (let i = 0; i < queries.length; i++) {
+        let a = queries[i][0];
+        let b = queries[i][1];
+        let k = queries[i][2];
+
+        outputArray[a] += k;
+        outputArray[b + 1] -= k;
+    }
+
+    //Do Prefix Summ P = previous + current number
+    for (let j = 0; j < outputArray.length; j++) {
+
+        summ = summ + outputArray[j];
+
+        if (summ > max) {
+            max = summ;
+        }
+    }
+
+    return max;
 }
+
+let queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]];
+let n = 5;
+
+arrayManipulation(n, queries);
